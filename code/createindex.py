b5a6c1f9fc2e84f52567355cf01994e8c729293a
Jonathan Salzer <jonathan@salzer.net>
2020-12-05 19:27:29 +0100

    first commit

---
+++ code/createindex.py
@@ -0,0 +1,12 @@
+"""
+This file contains your code to create the inverted index. Besides implementing and using the predefined tokenization function (text2tokens), there are no restrictions in how you organize this file.
+predefined functions if available.
+"""
+
+
+def text2tokens(text):
+    """
+    :param text: a text string
+    :return: a tokenized string with preprocessing (e.g. stemming, stopword removal, ...) applied
+    """
+    pass
---
+++ retrieval_results/Readme.txt
@@ -0,0 +1,6 @@
+This folder should contain the results of your retrieval runs.
+There should be four runs (and therefore four files) in the TREC format:
+- TFIDF on the evaluation-set
+- TFIDF on the dev-set
+- BM25 on the evaluation-set
+- BM25 on the dev-set
